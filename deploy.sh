da51086222d062abbed1056ea47fe996a7c9006e
Sully Sullenberger <sully@sadburger.com>
2023-06-22 15:59:50 -0700

    minor deploy tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 source prod.env.sh
 echo $DOMAIN
-git pull && docker build . -t emilytarot:latest && docker stack deploy -c docker-compose.prod.yaml emilytarot && docker service update emilytarot_emilytarot --force
+git pull && docker build . -t personal-streamlit-dash:latest && docker stack deploy -c docker-compose.prod.yaml personaldash && docker service update personaldash_dashboardui --force
 
 docker ps
